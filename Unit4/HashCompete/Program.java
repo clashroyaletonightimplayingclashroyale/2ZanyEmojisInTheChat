13f5aabeb85da260b740afcf778e82ada9f72e0e
FireNaruto3 <jonathanlin2020@gmail.com>
2025-02-24 09:56:00 -0800

    har har har har

---
+++ Unit4/HashCompete/Program.java
@@ -0,0 +1,27 @@
+package Unit4.HashCompete;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    private static final String _bookFile = "Unit4/HashCompete/pride_and_prejudice.txt";
+    
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello to the Hashing competition!");
+        
+        // read the book word by word and add each word to a HashWordSet
+        HashWordSet hash = new HashWordSet();
+        File file = new File(_bookFile);
+        Scanner reader = new Scanner(file, "UTF-8");
+        while(reader.hasNext()) {
+            hash.add(reader.next());
+        }
+        reader.close();
+        
+        // print the hash object
+        System.out.println(hash);
+        
+        System.out.println("Goodbye!");
+    }
+}
--- Unit4/Program.java
+++ Unit4/Program.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 
 
@@ -11,16 +12,42 @@ public class Program {
 
     public static void main(String[] args) throws IOException{
 
-        String link = "gutenberg.org/cache/epub/103/pg103.txt";
+        String link = "https://www.gutenberg.org/cache/epub/103/pg103.txt";
 
         @SuppressWarnings("deprecation")
         URL url = new URL(link);
         URLConnection urlClnx = url.openConnection();
         InputStream urlStream = urlClnx.getInputStream();
-
+        
+        Scanner urlScan = new Scanner(urlStream);
         HashStringSet set = new HashStringSet();
-        set.printInfo();
 
+        while(urlScan.hasNext()){
+            String curWord = urlScan.next().toLowerCase();
+            if(containsSpecialCharacters(curWord)){
+                removeSpecialCharacters(curWord);
+            }
+            set.add(curWord);
+            
+        }
+
+        set.printInfo();
+        urlScan.close();
+
+    }
+
+    public static boolean containsSpecialCharacters(String input) {
+        String specialCharacters = "!@#$%^&*()_+-=[]{}|;:',.<>?/~`";
+        for (int i = 0; i < specialCharacters.length(); i++) {
+            if (input.contains(String.valueOf(specialCharacters.charAt(i)))) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+    
+    public static String removeSpecialCharacters(String input) {
+        return input.replaceAll("[^a-zA-Z0-9]", "");
     }
     
 }
